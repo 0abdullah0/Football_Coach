b4f8e49ae342b94fbe4a3468523ce6db0a687619
abdelrhman <abdo_201333@hotmail.com>
2020-06-07 01:45:31 +0200

    add new data base connection  and edit app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var bodyParser = require('body-parser')
+var historyRouter = require('./routes/History')
+var scoreRouter = require('./routes/score')
 
 var registerRouter = require('./routes/Authenticate/register');
 var loginRouter= require('./routes/Authenticate/login');
@@ -15,6 +17,8 @@ var app = express();
 var server = app.listen(process.env.PORT||8080, function() {
   console.log('Ready on port %d', server.address().port);
 });
+
+app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/authenticate', registerRouter);
 app.use('/authenticate', loginRouter);
 app.use('/authenticate', logoutRouter);
+app.use('/history', historyRouter);
+app.use('/score',scoreRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
